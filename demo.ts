1d442db8df39d9d2d7d670cf93e031c3ef027853
vishesh-baghel <visheshbaghel99@gmail.com>
2023-12-28 18:17:43 +0530

    fixed bug changed demo.ts

--- demo.ts
+++ demo.ts
@@ -27,4 +27,33 @@ function userCode() {
   return {
       fullName: firstName + ", " + lastName
   }
+}function userCode() {
+  const Email = {{request.body.email}}
+  const fname = {{request.body.firstName}}
+  const lname = {{request.body.lastName}}
+// one bug comment
+  if (!{{request.body.email}}) {
+      return {
+          status: 400,
+          error: "missing email from request body"
+      }
+  }
+
+  if (!{{request.body.firstName}}) {
+      return {
+          status: 400,
+          error: "missing first name from the request body"
+      }
+  }
+
+  if (!{{request.body.lastName}}) {
+      return {
+          status: 400,
+          error: "missing last name from the request body"
+      }
+  }
+
+  return {
+      fullName: firstName + ", " + lastName
+  }
 }
