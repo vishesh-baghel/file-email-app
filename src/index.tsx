4836513aaac68905be87a7bfd23de3c350417132
vishesh-baghel <visheshbaghel99@gmail.com>
2024-01-07 19:57:41 +0530

    renamed bug file

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"; // one more comment
 // one more conmment
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
