09fdd6ebc4d7ae48d556e4feda45299250cb3cd2
vishesh-baghel <visheshbaghel99@gmail.com>
2024-01-07 11:48:54 +0530

    fixed all tests

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,7 @@ const root = ReactDOM.createRoot(
 // one more please
 // that didn't work
 // one more comment
+// one more comment for tsitng
 root.render(
   <React.StrictMode>
     <BrowserRouter>
