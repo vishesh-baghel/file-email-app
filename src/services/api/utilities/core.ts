adb75c3ec52fed374b69f8dd601e9d2b87d88074
vishesh-baghel <visheshbaghel99@gmail.com>
2024-01-14 15:19:31 +0530

    fixed the bug

--- src/services/api/utilities/core.ts
+++ src/services/api/utilities/core.ts
@@ -48,3 +48,4 @@ export class ApiCore {
     }
   }
 }
+// comment for bugs
--- src/services/api/utilities/provider.ts
+++ src/services/api/utilities/provider.ts
@@ -23,7 +23,6 @@ async function post<T>(resource: string, model: T): Promise<AxiosResponse> {
     .then(handleResponse)
     .catch(handleError);
 }
-//m ore
 async function put<T>(
   resource: string,
   model: T,
@@ -46,6 +45,7 @@ async function removeMultiple(
   resource: string,
   ids: string[]
 ): Promise<AxiosResponse> {
+  // more comment
   return axios
     .delete(`${BASE_URL}/${resource}`, { data: { ids } })
     .then(handleResponse)
